851711e7b1fd3946c4587bfc1e5ff05f0671a9d7
Luis Felipe <luisfelipe192ss@gmail.com>
2026-05-06 15:54:27 -0300

    Separa renderização e movimentação em funções

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void renderizarMAPA(char mapa[5][12], int andarX, int andarY){
+    for(int y=0;y<5;y++){
+            for(int x=0;x<11;x++){
+                if(x == andarX && y == andarY){
+                    printf("P");
+                } 
+                else{
+                    printf("%c", mapa[y][x]);
+                }
+            }
+            printf("\n");
+        }
+    }
+
+void moverjogador(char mapa[5][12], int *andarX, int *andarY){
+    char comando;
+    
+    printf("wasd:");
+    scanf(" %c", &comando);
+
+    if(comando == 'w'){
+        if(mapa[*andarY-1][*andarX] != '#'){
+            (*andarY)--;
+        }
+    }
+    else if(comando == 's'){
+        if(mapa[*andarY+1][*andarX] != '#'){
+            (*andarY)++;
+        }
+    }
+    else if(comando == 'a'){
+        if(mapa[*andarY][*andarX-1] != '#'){
+            (*andarX)--;
+        }
+    }
+    else if(comando == 'd'){
+        if(mapa[*andarY][*andarX+1] != '#'){
+            (*andarX)++;
+        }
+    } else{
+        printf("comando inválido!");
+    }
+}
+
 int main() {
 
     char comando;
@@ -18,44 +62,8 @@ int main() {
     while(1) {
         system("clear");
 
-        for(int y=0;y<5;y++){
-            for(int x=0;x<11;x++){
-                if(x == andarX && y == andarY){
-                    printf("P");
-                } 
-                else{
-                    printf("%c", mapa[y][x]);
-                }
-            }
-            printf("\n");
-        }
-
-        printf("wasd:");
-        scanf(" %c", &comando);
-
-        if(comando == 'w'){
-            if(mapa[andarY-1][andarX] != '#'){
-                andarY--;
-            }
-        }
-        else if(comando == 's'){
-            if(mapa[andarY+1][andarX] != '#'){
-                andarY++;
-            }
-        }
-
-        else if(comando == 'a'){
-            if(mapa[andarY][andarX-1] != '#'){
-                andarX--;
-            }
-        }
-        else if(comando == 'd'){
-            if(mapa[andarY][andarX+1] != '#'){
-            andarX++;
-            }
-        } else{
-            printf("comando inválido!");
-        }
-}
+        renderizarMAPA(mapa,andarX,andarY);
+        moverjogador(mapa,&andarX,&andarY);
+    }
     return 0;
 }
