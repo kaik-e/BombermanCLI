5daf8c5a342d96254c3010856f99080b77067ea7
Luis Felipe <luisfelipe192ss@gmail.com>
2026-05-11 17:37:05 -0300

    Refatora arquitetura do bomberman em funçoes

--- main.c
+++ main.c
@@ -21,249 +21,240 @@ typedef struct inimigo{
     float tempoInimigo;
 }inimigo;
 
-int main(){
+void moverjogador(char mapa[LINHAS][COLUNAS+1],int *andarX, int *andarY){
 
-    char mapa[LINHAS][COLUNAS+1]={
-    "#####################",
-    "#   *   *     ***   #",
-    "#  *    *  *  *     #",  
-    "#    *    *    *    #",
-    "#     **     *   *  #",
-    "#   * *   *         #",
-    "#    *  * *     *   #",
-    "#  *    *    *   *  #",
-    "# *  *    *  *  *   #",
-    "#*     *      *     #",
-    "#  *     *       *  #",
-    "#    *  *    *      #",
-    "#####################"
-    };
+    if(IsKeyPressed(KEY_W) && mapa[*andarY - 1][*andarX] != '#' && mapa[*andarY - 1][*andarX] != '*'){
+        (*andarY)--;
+    }
+    if(IsKeyPressed(KEY_S) && mapa[*andarY + 1][*andarX] != '#' && mapa[*andarY + 1][*andarX] != '*'){
+        (*andarY)++;
+    }
+    if(IsKeyPressed(KEY_A) && mapa[*andarY][*andarX - 1] != '#' && mapa[*andarY][*andarX - 1] != '*'){
+        (*andarX)--;
+    }
+    if(IsKeyPressed(KEY_D) && mapa[*andarY][*andarX + 1] != '#' && mapa[*andarY][*andarX + 1] != '*'){
+        (*andarX)++;
+    }
+}
 
-    int vivo =1;
+void moverinimigo(char mapa[LINHAS][COLUNAS+1],inimigo *inimigo1, int andarX, int andarY, int *vivo){
 
-    int andarX = 10;
-    int andarY = 6;
+    if(inimigo1->vivo == 0){
+        return;
+    }
 
-    bomba bomba1;
+    if(inimigo1->x == andarX && inimigo1->y == andarY){
+        *vivo = 0;
+    }
 
-    inimigo inimigo1;
-    inimigo1.x = 15;
-    inimigo1.y = 8;
-    inimigo1.vivo = 1;
-    inimigo1.tempoInimigo = 0;
+    inimigo1->tempoInimigo += GetFrameTime();
 
-    bomba1.ativa=0;
-    bomba1.explosao = 0;
+    if(inimigo1->tempoInimigo >= 0.5f){
 
-    InitWindow(COLUNAS*pixel, LINHAS*pixel, "BomberMan");
+        int direcao = rand()%4;
 
-    SetTargetFPS(60);
-
-    while(!WindowShouldClose() && vivo==1){
-
-        if(IsKeyPressed(KEY_W) && mapa[andarY-1][andarX] != '#' && mapa[andarY-1][andarX] != '*'){
-            andarY--;
-        }
-        if(IsKeyPressed(KEY_S) && mapa[andarY+1][andarX] != '#' && mapa[andarY+1][andarX] != '*'){
-            andarY++;
-        }
-        if (IsKeyPressed(KEY_A) && mapa[andarY][andarX-1] != '#' && mapa[andarY][andarX-1] != '*'){
-            andarX--;
-        }
-        if (IsKeyPressed(KEY_D) && mapa[andarY][andarX+1] != '#' && mapa[andarY][andarX+1] != '*'){
-            andarX++;   
+        if(direcao == 0 && mapa[inimigo1->y - 1][inimigo1->x] != '#' && mapa[inimigo1->y - 1][inimigo1->x] != '*'){
+            inimigo1->y--;
         }
 
-        if (IsKeyPressed(KEY_SPACE) && mapa[bomba1.y][bomba1.x] != '*') {
-
-            bomba1.x = andarX;
-            bomba1.y = andarY;
-            bomba1.ativa = 1;
-            bomba1.timer= 4.0f;
+        if(direcao == 1 && mapa[inimigo1->y+1][inimigo1->x] != '#' && mapa[inimigo1->y+1][inimigo1->x] != '*'){
+            inimigo1->y++;
         }
 
-         if (bomba1.ativa == 1){
-            bomba1.timer -= GetFrameTime();
+        if(direcao == 2 && mapa[inimigo1->y][inimigo1->x-1] != '#' && mapa[inimigo1->y][inimigo1->x-1] != '*'){
+            inimigo1->x--;
+        }
+
+        if(direcao == 3 && mapa[inimigo1->y][inimigo1->x+1] != '#' && mapa[inimigo1->y][inimigo1->x+1] != '*'){
+            inimigo1->x++;
+        }
+
+        if(inimigo1->x == andarX && inimigo1->y == andarY){
+            *vivo = 0;
+        }
+
+        inimigo1->tempoInimigo = 0;
+    }
+}
+
+void colocarbomba(bomba* bomba1, int andarX, int andarY){
+    if(IsKeyPressed(KEY_SPACE) && bomba1->ativa == 0){
+
+        bomba1->x = andarX;
+        bomba1->y = andarY;
+
+        bomba1->ativa = 1;
+
+        bomba1->timer = 4.0f;
+    }
+}
+
+void atualizarbomba(char mapa[LINHAS][COLUNAS + 1],bomba *bomba1,int andarX,int andarY,int *vivo,inimigo *inimigo1){
+
+    if (bomba1->ativa == 1){
+        bomba1->timer -= GetFrameTime();
             
-            if (bomba1.timer <= 0){
-                bomba1.ativa = 0;
+        if (bomba1->timer <= 0){
 
-                bomba1.explosao = 1;
+            bomba1->ativa = 0;
+            bomba1->explosao = 1;
+            bomba1->tempoExplosao = 0.5f;
 
-                bomba1.tempoExplosao = 0.5f;
+            // qubrar bloco com explosao
+            if(mapa[bomba1->y-1][bomba1->x] == '*'){
+                mapa[bomba1->y-1][bomba1->x] =' ';
+            }
 
-                if(mapa[bomba1.y-1][bomba1.x] == '*'){
-                    mapa[bomba1.y-1][bomba1.x]=' ';
-                } if (mapa[bomba1.y+1][bomba1.x] == '*'){
-                    mapa[bomba1.y+1][bomba1.x] = ' ';
-                } if(mapa[bomba1.y][bomba1.x-1] == '*'){
-                    mapa[bomba1.y][bomba1.x-1] = ' ';
-                } if(mapa[bomba1.y][bomba1.x+1] == '*'){
-                    mapa[bomba1.y][bomba1.x+1] = ' ';
-                }
+            if(mapa[bomba1->y+1][bomba1->x] == '*'){
+                mapa[bomba1->y+1][bomba1->x] = ' ';
+            }
 
-                if(bomba1.x ==  andarX && bomba1.y == andarY){
-                    vivo = 0;
-                } if(bomba1.x ==andarX && bomba1.y-1 == andarY){
-                    vivo = 0;
-                }if(bomba1.x ==andarX && bomba1.y+1 == andarY){
-                    vivo = 0;
-                } if(bomba1.x-1 == andarX && bomba1.y == andarY){
-                    vivo = 0;
-                } if(bomba1.x+1 == andarX && bomba1.y == andarY){
-                    vivo = 0;
-                }
+            if(mapa[bomba1->y][bomba1->x-1] == '*'){
+                mapa[bomba1->y][bomba1->x-1] =' ';
+            }
 
-                if (bomba1.x == inimigo1.x && bomba1.y == inimigo1.y){
-                    inimigo1.vivo = 0;
-                }
-                if (bomba1.x == inimigo1.x && bomba1.y-1 == inimigo1.y){
-                    inimigo1.vivo = 0;
-                }
-                if(bomba1.x == inimigo1.x && bomba1.y+1 == inimigo1.y){
-                    inimigo1.vivo = 0;
-                }
-                if(bomba1.x-1 == inimigo1.x && bomba1.y == inimigo1.y){
-                    inimigo1.vivo = 0;
-                }
-                if(bomba1.x+1 == inimigo1.x && bomba1.y == inimigo1.y){
-                    inimigo1.vivo = 0;
-                }
+            if(mapa[bomba1->y][bomba1->x+1] == '*'){
+                mapa[bomba1->y][bomba1->x+1] =' ';
+            }
+
+            // matar jogador
+            if(bomba1->x == andarX && bomba1->y == andarY){
+                *vivo = 0;
+            }
+            if(bomba1->x == andarX && bomba1->y-1 == andarY){
+                *vivo=0;
+            }
+            if(bomba1->x == andarX && bomba1->y + 1 == andarY){
+                *vivo=0;
+            }
+            if(bomba1->x-1 == andarX && bomba1->y == andarY){
+                *vivo=0;
+            }
+            if(bomba1->x+1 == andarX && bomba1->y == andarY){
+                *vivo=0;
+            }
+
+            // matar inimigo
+            if(bomba1->x == inimigo1->x && bomba1->y == inimigo1->y){
+                inimigo1->vivo = 0;
+            }
+            if(bomba1->x == inimigo1->x && bomba1->y-1 == inimigo1->y){
+                inimigo1->vivo =0;
+            }
+            if(bomba1->x == inimigo1->x && bomba1->y+1 == inimigo1->y){
+                inimigo1->vivo =0;
+            }
+            if(bomba1->x-1 == inimigo1->x && bomba1->y == inimigo1->y){
+                inimigo1->vivo = 0;
+            }
+            if(bomba1->x+1 == inimigo1->x && bomba1->y == inimigo1->y){
+                inimigo1->vivo =0;
             }
         }
+    }
 
-        if (bomba1.explosao == 1){
-            bomba1.tempoExplosao -= GetFrameTime();
+    if (bomba1->explosao == 1){
+        bomba1->tempoExplosao -= GetFrameTime();
             
-            if (bomba1.tempoExplosao <= 0){
-                bomba1.explosao=0;
-            }
+        if (bomba1->tempoExplosao <= 0){
+            bomba1->explosao = 0;
         }
+    }
+}
 
-        inimigo1.tempoInimigo += GetFrameTime();
-
-        if(inimigo1.tempoInimigo >= 0.5f){
-            int direcao = rand() % 4;
-
-            if(direcao == 0 && mapa[inimigo1.y-1][inimigo1.x] != '#' && mapa[inimigo1.y-1][inimigo1.x] != '*'){
-                inimigo1.y--;
+void dmapa(char mapa[LINHAS][COLUNAS+1]){
+    for (int y=0;y < LINHAS;y++){
+        for (int x=0;x < COLUNAS;x++){
+            // parede
+            if (mapa[y][x]== '#') {
+                DrawRectangle(
+                    x * pixel,
+                    y * pixel,
+                    pixel,
+                    pixel,
+                    GRAY
+                );
             }
-
-            if(direcao == 1 && mapa[inimigo1.y+1][inimigo1.x] != '#' && mapa[inimigo1.y+1][inimigo1.x] != '*'){
-                inimigo1.y++;
+            // bloco que qubra
+            if (mapa[y][x] == '*'){
+                DrawRectangle(
+                    x * pixel,
+                    y * pixel,
+                    pixel,
+                    pixel,
+                    BROWN
+                );
             }
+}}}
 
-            if(direcao == 2 && mapa[inimigo1.y][inimigo1.x-1] != '#' && mapa[inimigo1.y][inimigo1.x-1] != '*'){
-                inimigo1.x--;
-            }
+void djogador(int x,int y){
+    DrawRectangle(
+        x * pixel,
+        y * pixel,
+        pixel,
+        pixel,
+        RED
+    );
+}
 
-            if(direcao == 3 && mapa[inimigo1.y][inimigo1.x+1] != '#' && mapa[inimigo1.y][inimigo1.x+1] != '*'){
-                inimigo1.x++;
-            }
-            inimigo1.tempoInimigo = 0;
-        }
+void dbomba(bomba bomba1){
+    if (bomba1.ativa == 1){
+        DrawRectangle(
+            bomba1.x * pixel,
+            bomba1.y * pixel,
+            pixel,
+            pixel,
+            BLUE
+        );
+    }
+}
 
-        if(inimigo1.x == andarX && inimigo1.y == andarY){
-            vivo = 0;
-        }
+void dexplosao(bomba bomba1){
+    if (bomba1.explosao == 1){
+        DrawRectangle(
+            bomba1.x * pixel,
+            bomba1.y * pixel,
+            pixel,
+            pixel,
+            YELLOW
+        );
 
-        BeginDrawing();
-        ClearBackground(BLACK);
+        DrawRectangle(
+            bomba1.x * pixel,
+            (bomba1.y-1) * pixel,
+            pixel,
+            pixel,
+            ORANGE
+        );
 
-        for (int y = 0; y < LINHAS; y++) {
-            for (int x = 0; x < COLUNAS; x++) {
+        DrawRectangle(
+            bomba1.x * pixel,
+            (bomba1.y+1) * pixel,
+            pixel,
+            pixel,
+            ORANGE
+        );
 
-                if (mapa[y][x] == '#') {
-                    DrawRectangle(
-                        x * pixel,
-                        y * pixel,
-                        pixel,
-                        pixel,
-                        GRAY
-                    );
-                }
+        DrawRectangle(
+            (bomba1.x-1) * pixel,
+            bomba1.y * pixel,
+            pixel,
+            pixel,
+            ORANGE
+        );
 
-                if (mapa[y][x] == '*') {
-                    DrawRectangle(
-                        x * pixel,
-                        y * pixel,
-                        pixel,
-                        pixel,
-                        BROWN
-                    );
-                }
+        DrawRectangle(
+            (bomba1.x+1) * pixel,
+            bomba1.y * pixel,
+            pixel,
+            pixel,
+            ORANGE
+        );
+    }
+}
 
-                if (x == andarX && y == andarY) {
-
-                    DrawRectangle(
-                        x * pixel,
-                        y * pixel,
-                        pixel,
-                        pixel,
-                        RED
-                    );
-                }
-            }
-        }
-
-        if (bomba1.ativa==1){
-
-            DrawRectangle(
-                bomba1.x * pixel,
-                bomba1.y * pixel,
-                pixel,
-                pixel,
-                BLUE
-            );
-        }
-
-        if (bomba1.explosao==1){
-            DrawRectangle(
-                bomba1.x *pixel,
-                bomba1.y*pixel,
-                pixel,
-                pixel,
-                YELLOW
-            );
-
-
-            DrawRectangle(
-                bomba1.x*pixel,
-                (bomba1.y-1)*pixel,
-                pixel,
-                pixel,
-                ORANGE
-            );
-
-            DrawRectangle(
-                bomba1.x* pixel,
-                (bomba1.y+1) * pixel,
-                pixel,
-                pixel,
-                ORANGE
-            );
-
-
-            DrawRectangle(
-                (bomba1.x-1)*pixel,
-                bomba1.y*pixel,
-                pixel,
-                pixel,
-                ORANGE
-            );
-
-
-            DrawRectangle(
-                (bomba1.x+1)*pixel,
-                bomba1.y*pixel,
-                pixel,
-                pixel,
-                ORANGE
-            );
-        }
-
-        if(inimigo1.vivo == 1){
-
+void dinimigo(inimigo inimigo1){
+    if(inimigo1.vivo == 1){
         DrawRectangle(
             inimigo1.x * pixel,
             inimigo1.y * pixel,
@@ -272,6 +263,74 @@ int main(){
             GREEN
         );
     }
+}
+
+int main(){
+
+    char mapa[LINHAS][COLUNAS+1]={
+        "#####################",
+        "#   *   *     ***   #",
+        "#  *    *  *  *     #",
+        "#    *    *    *    #",
+        "#     **     *   *  #",
+        "#   * *   *         #",
+        "#    *  * *     *   #",
+        "#  *    *    *   *  #",
+        "# *  *    *  *  *   #",
+        "#*     *      *     #",
+        "#  *     *       *  #",
+        "#    *  *    *      #",
+        "#####################"
+    };
+
+    int vivo = 1;
+
+    int andarX = 10;
+    int andarY = 6;
+
+    bomba bomba1;
+
+    bomba1.ativa = 0;
+    bomba1.explosao = 0;
+
+    inimigo inimigo1;
+
+    inimigo1.x = 15;
+    inimigo1.y = 8;
+    inimigo1.vivo = 1;
+    inimigo1.tempoInimigo = 0;
+
+    InitWindow(
+        COLUNAS * pixel,
+        LINHAS * pixel,
+        "BomberMan"
+    );
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose() && vivo == 1){
+
+        moverjogador(mapa,&andarX,&andarY);
+
+        colocarbomba(&bomba1, andarX, andarY);
+
+        atualizarbomba(mapa,&bomba1,andarX,andarY,&vivo,&inimigo1);
+
+        moverinimigo(mapa,&inimigo1, andarX,andarY, &vivo);
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        dmapa(mapa);
+
+        djogador(andarX,andarY);
+
+        dbomba(bomba1);
+
+        dexplosao(bomba1);
+
+        dinimigo(inimigo1);
+
         EndDrawing();
     }
 
